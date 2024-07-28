6ba8f2823e70d6a81189b5223abc23bcdf730fc9
jhw3355 <jhwang0324@gmail.com>
2024-07-28 14:09:34 +0800

    added up to 540 qns

--- src/components/AllContext.jsx
+++ src/components/AllContext.jsx
@@ -2,59 +2,58 @@ import { createContext, useState, useEffect, useContext } from "react";
 import { Container, Spinner } from "react-bootstrap";
 import { shuffle } from "d3-array";
 
-const jsonSource = "https://gist.githubusercontent.com/wjh3355/85ea89c3330149e56c71002dc8b1aad2/raw/1017c63f8ccfa0dfefa7e188c5025fd3083fa662/source.json";
+const jsonSource =
+   "https://gist.githubusercontent.com/wjh3355/0044ee12436ff44915daf15e45622ef2/raw/be0c6588a1cbc2c6632924a2b97211a0ac41b8f2/source.json";
 
 const AppContext = createContext();
 export const useAppContext = () => useContext(AppContext);
 
 export function AppProvider({ children }) {
-   const [qnObjArr, setQnObjArr]                         = useState(null);
-   const [qnNum, setQnNum]                               = useState(0);
-   const [qnObj, setQnObj]                               = useState(null);
-   const [numQnsAns, setNumQnsAns]                       = useState(0);
-   const [numCorrectAns, setNumCorrectAns]               = useState(0);
-   const [isNextQnBtnDisabled, setIsNextQnBtnDisabled]   = useState(true);
-   const [isExplBtnDisabled, setIsExplBtnDisabled]       = useState(true);
-   const [isCorrect, setIsCorrect]                       = useState(null);
-   const [wrongAnsArr, setWrongAnsArr]                   = useState([]);
+   const [qnObjArr, setQnObjArr] = useState(null);
+   const [qnNum, setQnNum] = useState(0);
+   const [qnObj, setQnObj] = useState(null);
+   const [numQnsAns, setNumQnsAns] = useState(0);
+   const [numCorrectAns, setNumCorrectAns] = useState(0);
+   const [isNextQnBtnDisabled, setIsNextQnBtnDisabled] = useState(true);
+   const [isExplBtnDisabled, setIsExplBtnDisabled] = useState(true);
+   const [isCorrect, setIsCorrect] = useState(null);
+   const [wrongAnsArr, setWrongAnsArr] = useState([]);
 
    useEffect(() => {
       fetch(jsonSource)
          .then((response) => response.json())
          .then((data) => {
             const shuffledQnArray = shuffle([...data]);
-            const shuffledQnOrder = shuffledQnArray.map(obj => obj.qnNum);
+            const shuffledQnOrder = shuffledQnArray.map((obj) => obj.qnNum);
             setQnObjArr(shuffledQnArray);
             console.log("Questions fetched successfully");
-            console.log('Question order:', shuffledQnOrder.join(', '))
+            console.log("Question order:", shuffledQnOrder.join(", "));
+            console.log('Total num of qns:', shuffledQnOrder.length);
             // Not necessary
          })
          .catch((error) =>
             console.log("Error when fetching questions!", error)
-      );
+         );
    }, []);
-   
+
    useEffect(() => {
       qnObjArr && setQnObj(qnObjArr[qnNum]);
-      console.log(qnNum, qnObjArr);
    }, [qnNum, qnObjArr]);
    // if qnObjArr is not null, set qnObj once qnNum changes
-
-   console.log('%cOOOOOOOOOOOOOOOOOOOOOo', 'background: red');
 
    function handleOptionClick(isCorrectOption) {
       setIsNextQnBtnDisabled(false);
       setIsExplBtnDisabled(false);
       setIsCorrect(isCorrectOption);
 
-      console.log("Answer received:", isCorrectOption ? 'Correct!' : 'Wrong!');
+      console.log("Answer received:", isCorrectOption ? "Correct!" : "Wrong!");
    }
 
    function handleNextQnBtnClick() {
-      setNumQnsAns(prevNum => prevNum + 1);
-      isCorrect 
-         ? setNumCorrectAns(prevNum => prevNum + 1)
-         : setWrongAnsArr(prevArr => [...prevArr, qnObj]);
+      setNumQnsAns((prevNum) => prevNum + 1);
+      isCorrect
+         ? setNumCorrectAns((prevNum) => prevNum + 1)
+         : setWrongAnsArr((prevArr) => [...prevArr, qnObj]);
       // before we update isCorrect back to null, increment numCorrectAns if it is true, if not add the qnObj to wrongQnsArr
 
       setIsNextQnBtnDisabled(true);
@@ -75,7 +74,7 @@ export function AppProvider({ children }) {
    }
 
    return (
-      <AppContext.Provider 
+      <AppContext.Provider
          value={{
             qnObj,
             handleOptionClick,
@@ -85,10 +84,10 @@ export function AppProvider({ children }) {
             handleNextQnBtnClick,
             numQnsAns,
             numCorrectAns,
-            wrongAnsArr
+            wrongAnsArr,
          }}
       >
-         {!qnObj ? <DisplayLoading />: children}
+         {!qnObj ? <DisplayLoading /> : children}
       </AppContext.Provider>
    );
 }
