5e77255cff2874e99083479ea2b54735a2365721
jhw3355 <jhwang0324@gmail.com>
2024-07-29 11:16:25 +0800

    resolved homepage issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import NavigationBar from "./components/header/NavigationBar";
 import Home from "./components/Home";
 import GEPApp from "./components/GEP MCQ/GEPApp";
+import NotFound from "./components/NotFound";
 
 import { Routes, Route } from "react-router-dom";
 
@@ -12,6 +13,7 @@ export default function App() {
          <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/gep_mcq" element={<GEPApp />} />
+            <Route path="*" element={<NotFound />} />
          </Routes>
       </>
    );
---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+export default function NotFound() {
+   return(
+      <>
+         <Container className="d-flex justify-content-center my-3">
+            <h2>404: Page Not Found</h2>
+         </Container>
+      </>
+   );
+}
